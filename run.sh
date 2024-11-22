e54383833d5e37f2a4e9456a59a01aeffeaa860d
Rajneesh Bansal <rajneeshb231@gmail.com>
2024-11-22 23:53:40 +0530

    Updated run.sh and stop.sh for better user experience

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # This script builds and runs the Docker container locally
-
+echo "Your Application is starting."
+echo "Please be Patient, this could take a few minutes if the Image is not present on your system."
 # Pull the prebuilt Docker image
-docker pull rajneesh768/word2pdf
+docker pull rajneesh768/word2pdf >/dev/null 2>&1
 
-# Run the container (expose port 3000)
-docker run -d -p 3000:3000 rajneesh768/word2pdf
-echo "Container Started"
-echo "Server Running on port 3000"
+# Run the container (expose port 3000) in detached mode, suppress all output
+docker run -d -p 3000:3000 rajneesh768/word2pdf >/dev/null 2>&1
+
+# Check if the docker run command was successful
+if [ $? -eq 0 ]; then
+  echo "Container Started"
+  echo "Server Running on port 3000"
+else
+  echo "Failed to start the container. Try restarting your system"
+fi
--- stop.sh
+++ stop.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker rm $(docker stop $(docker ps -a -q  --filter ancestor=rajneesh768/word2pdf:latest))
+
+# Stop and remove all containers running the specified image, suppress all output
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=rajneesh768/word2pdf:latest) 2>/dev/null) >/dev/null 2>&1
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+  echo "Application Stopped"
+  echo "You may exit the console"
+else
+  echo "There are no Applications running"
+fi
